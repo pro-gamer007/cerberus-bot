bbbfbdd44059ae65e608fd78c249d4b7c0055ed9
pro_gamer007 <coolaryan@icloud.com>
2021-05-05 22:47:18 +0530

    oop

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,7 @@ module.exports.run = async (client, message, args) => {
 			{ name: 'Add - Adds a new tag', value: 'Usage: $add {tag name} {tag response}' },
 			{ name: 'Delete - Deletes a tag', value: 'Usage: $delete {tag name}' },
 			{ name: 'Edit - Edits a tag', value: 'Usage: $edit {tag name} -{type} (name/response) {new name or response}' },
-			{ name: 'All - Shows all the responses', value: 'Usage: $responses' },
+			{ name: 'All - Shows all the responses', value: 'Usage: $all' },
 			{ name: 'Ping - Shows the bots ping', value: 'Usage: $ping' },
 		)
 		.setFooter('Dont include () or {} in cmds! | () is for options');
