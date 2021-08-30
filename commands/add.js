79098f0c3871c9d03b45bbc33796faa2be74e143
pro_gamer007 <66844505+pro-gamer007@users.noreply.github.com>
2021-08-30 14:54:33 +0530

    make it so namby can use

--- commands/add.js
+++ commands/add.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
 	const adminperms = new Discord.Permissions('ADMINISTRATOR');
-	if (!message.member.hasPermission(adminperms)) return message.reply('You arent allowed to do that!');
+	if (!message.member.hasPermission(adminperms) && message.author.id != '827265967729279068') return message.reply('You arent allowed to do that!');
 	if (!args[0]) return message.channel.send('Please tell me a name for this tag.');
 	if (!args[1]) return message.channel.send('Please tell me a response for this tag.');
 	const tag = await client.data.addTag(args[0].toLowerCase(), args.slice(1).join(' '));
