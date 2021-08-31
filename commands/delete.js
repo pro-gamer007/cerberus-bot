318c0171d0684c5b0488177e01d41808765463ec
pro_gamer007 <coolaryan@icloud.com>
2021-08-31 19:54:57 +0530

    allow namby to use all cmds

--- commands/delete.js
+++ commands/delete.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
 	const adminperms = new Discord.Permissions('ADMINISTRATOR');
-	if (!message.member.hasPermission(adminperms)) return message.reply('You arent allowed to do that!');
+	if (!message.member.hasPermission(adminperms) && message.author.id != '827265967729279068') return message.reply('You arent allowed to do that!');
 	if (!args[0]) return message.channel.send('Please give a tag name.');
 	const deleteTag = await client.data.deleteTag(args[0]);
 	message.channel.send(deleteTag);
